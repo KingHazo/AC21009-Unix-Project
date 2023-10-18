261c0c0d276598cd42c8febddc001704136b0006
Danyl Kecha <kechaofficial@gmail.com>
2023-10-18 09:39:56 +0100

    added archiving and reverting changes

--- gip.sh
+++ gip.sh
@@ -22,21 +22,22 @@ function init() {
     mkdir -p "$GIP_DIR/locks"
     mkdir -p "$GIP_DIR/archives"
 
-    #create file for logging user actions
+    # Create file for logging user actions
     touch "$MAIN_DIR/$GIP_DIR/log"
     log "$USER created project repository, and is now administrator of the project"
 
-    #create workspace area
+    # Create workspace area
     mkdir "$WORKSPACES_DIR"
     mkdir "$WORKSPACES_DIR/$USER"
 
     echo "Project repository is successfully initialised"
+    archive
 
-    #change security permissions to only allow the administrator write permissions
+    # Change security permissions to only allow the administrator write permissions
     chmod 755 "$GIP_DIR"
 }
 
-#takes in one parameter as input and appends it to the log file on a new line with a timestamp
+# Takes in one parameter as input and appends it to the log file on a new line with a timestamp
 function log() {
     local timestamp=$(date +"%Y-%m-%d %T")
     local log_file="$GIP_DIR/log"
@@ -57,6 +58,85 @@ function show_repo() {
     echo "---------------------"
 }
 
+#if a user needs a file for a program but does not want to check it out for editing
+#they can use gip link to have a link to the file in their workspace
+#takes a file name as input and links it to the calling user's workspace
+function pull() {
+    local file_name="$1"
+	#if the file doesn't exist, cannot link to it
+	if [ ! -e $file_name ]; then
+	    echo "File '$file_name' not found and cannot link to it"
+	    return 1
+	fi
+
+	#if a link already exists, cannot link to it
+	if [ -h "$WORKSPACES_DIR/$USER/$file_name" ]; then
+	    echo "Link already exists in your workspace"
+	    return 1
+	fi
+
+	ln "./$file_name" "$WORKSPACES_DIR/$USER/$file_name"
+}
+
+#takes in one input in the form of a filename and tries to remove that file
+function remove() {
+    local file_name="$1"
+    #if the file is currently checked out, abort
+    local lock_file="$GIP_DIR/locks/$file_name.lock"
+
+    if [ -f $lock_file ]; then
+	    local result=$(ls -l $GIP_DIR/locks/ | grep ".*$file_name\.lock$" | cut -d " " -f 3)
+	    echo "File '$file_name' is currently being edited by $result and thus cannot be removed"
+        return 1
+    fi
+
+    #if the file does not exist, cannot erase if
+    if [ ! -e $file_name ]; then
+	    echo "File '$file_name' does not exist"
+        return 1
+    fi
+
+    archive
+    rm $file_name
+    if [ $# -eq 3 ]; then
+        log "File '$file_name' has been removed by $USER\nCommit message: '$3'"
+    else
+        log "File '$file_name' has been removed by $USER"
+    fi
+}
+
+function checkout_file() {
+    local file_name="$1"
+    local lock_file="$GIP_DIR/locks/$file_name.lock"
+    # If file does not exist tell the user
+    if [ ! -e $file_name ]; then
+	    echo "File '$file_name' does not exist in project directory"
+	    return 1
+    fi
+    
+    if [ -e "$lock_file" ]; then
+	    local result=$(ls -l $GIP_DIR/locks/ | grep ".*$file_name\.lock$" | cut -d " " -f 3)
+	    echo "File '$file_name' is already being edited by $result"
+	    return 1
+    fi
+    
+    archive
+    # Create the lock file to indicate that the file is being edited and make a copy in the users workspace
+    touch "$lock_file"
+    echo $USER > $lock_file
+    
+    if [ -h "$WORKSPACES_DIR/$USER/$file_name" ]; then
+	    rm "$WORKSPACES_DIR/$USER/$file_name"
+    fi
+    
+    cp "$file_name" "$WORKSPACES_DIR/$USER/$file_name" 2>/dev/null
+    if [ $# -eq 3 ]; then
+        log "$USER has checked out '$file_name' for editing\nCommit message: '$3'"
+    else
+        log "$USER has checked out '$file_name' for editing."
+    fi
+}
+
 #takes a file name as input and either commits the changes you've made to said file in your directory and unlocks
 #it's usage for others or it creates a blank file in the project directory
 function checkin_file() {
@@ -66,6 +146,7 @@ function checkin_file() {
     #if file doesn't exist, inform the user or create it if a flag has been passed to create a file
     if [ ! -e $file_name ]; then
         if [ $newfile -eq 0 ]; then
+            archive
             touch $file_name;
             if [[ -z "$message" ]]; then
                 log "$USER has checked in new file '$file_name'"
@@ -87,6 +168,7 @@ function checkin_file() {
 	        echo "File is checked out by a different user"
         fi
         # Remove the lock file to indicate that the file is no longer being edited
+        archive
         rm "$lock_file"
 	    changes=$(diff -B $file_name "$WORKSPACES_DIR/$USER/$file_name" | tail -n +2 | grep -v "No newline at end of file")
 	    mv "$WORKSPACES_DIR/$USER/$file_name" "./$file_name"
@@ -97,82 +179,6 @@ function checkin_file() {
         fi
     else
         echo "File '$file_name' is not checked out"
-    fi
-}
-
-#if a user needs a file for a program but does not want to check it out for editing
-#they can use gip link to have a link to the file in their workspace
-#takes a file name as input and links it to the calling user's workspace
-function pull() {
-	#if the file doesn't exist, cannot link to it
-	if [ ! -e $1 ]; then
-	    echo "File '$1' not found and cannot link to it"
-	    return 1
-	fi
-
-	#if a link already exists, cannot link to it
-	if [ -h "$WORKSPACES_DIR/$USER/$1" ]; then
-	    echo "Link already exists in your workspace"
-	    return 1
-	fi
-
-	ln "./$1" "$WORKSPACES_DIR/$USER/$1"
-}
-
-#takes in one input in the form of a filename and tries to remove that file
-function remove() {
-    local file_name="$1"
-    #if the file is currently checked out, abort
-    local lock_file="$GIP_DIR/locks/$file_name.lock"
-
-    if [ -f $lock_file ]; then
-	    local result=$(ls -l $GIP_DIR/locks/ | grep ".*$file_name\.lock$" | cut -d " " -f 3)
-	    echo "File '$file_name' is currently being edited by $result and thus cannot be removed"
-        return 1
-    fi
-
-    #if the file does not exist, cannot erase if
-    if [ ! -e $file_name ]; then
-	    echo "File '$file_name' does not exist"
-        return 1
-    fi
-
-    rm $file_name
-    if [ $# -eq 3 ]; then
-        log "File '$file_name' has been removed by $USER\nCommit message: '$3'"
-    else
-        log "File '$file_name' has been removed by $USER"
-    fi
-}
-
-
-function checkout_file() {
-    local lock_file="$GIP_DIR/locks/$1.lock"
-
-    # If file does not exist tell the user
-    if [ ! -e $1 ]; then
-	    echo "File '$1' does not exist in project directory"
-	    return 1
-    fi
-    
-    if [ -e "$lock_file" ]; then
-	    local result=$(ls -l $GIP_DIR/locks/ | grep ".*$1\.lock$" | cut -d " " -f 3)
-	    echo "File '$1' is already being edited by $result"
-	    return 1
-    fi
-    # Create the lock file to indicate that the file is being edited and make a copy in the users workspace
-    touch "$lock_file"
-    echo $USER > $lock_file
-    
-    if [ -h "$WORKSPACES_DIR/$USER/$1" ]; then
-	    rm "$WORKSPACES_DIR/$USER/$1"
-    fi
-    
-    cp $1 "$WORKSPACES_DIR/$USER/$1"
-    if [ $# -eq 3 ]; then
-        log "$USER has checked out '$1' for editing\nCommit message: '$3'"
-    else
-        log "$USER has checked out '$1' for editing."
     fi
 }
 
@@ -187,19 +193,59 @@ function edit_file() {
     nano "$file_name"
 }
 
+function archive() {
+    local timestamp=$(date +"%Y-%m-%d %T")
+    local archive_name="$GIP_DIR/archives/$timestamp"
+    if [ -n "$(find "$MAIN_DIR" -mindepth 1 -type f -o -mindepth 1 -type d ! -name .gip ! -name workspaces)" ]; then
+        zip -r -y "$archive_name" "$MAIN_DIR" -x "$GIP_DIR/*" -x "$WORKSPACES_DIR/*" > /dev/null
+        log "$USER has archived the project repository under the following archive name: '$archive_name.zip'"
+    fi
+}
+
+function show_archives() {
+    local archives="$GIP_DIR/archives"
+    cd "$archives"
+    echo "Archives:"
+    ls | sed 's/^/* /' | sort
+    echo "---------------------"
+}
+
+# Revert changes based on archived files
+function revert() {
+    # revert to latest archive
+    local archive_name=""
+    if [ "$1" = "-l" ]; then
+        archive_name=$(ls -t "$GIP_DIR/archives" | head -n 1)
+    else
+        archive_name="$1"
+    fi
+    local archive_path="$GIP_DIR/archives/$archive_name"
+    if [ ! -e "$archive_path" ]; then
+        echo "Archive '$archive_name' does not exist"
+        return 1
+    fi
+    archive
+    find . -type f -not -path '*/\.*' -not -path './workspaces/*' -delete
+    unzip "$archive_path" -d "$MAIN_DIR" > /dev/null
+    log "$USER has reverted the project repository to the following version: '$archive_name'"
+    echo "Project repository has been reverted to the following version: '${archive_name%.*}'"
+}
 
 #This may be completely atypical to the rest of the design but bear with me (I secretly do not know what I'm doing)
 #File name given as variable $3, path as $2
 function import() {
-    echo "Importing file $3 from filepath $2"
+    local file_path="$1"
+    local file_name="$2"
+    echo "Importing file $file_name from filepath $file_path"
     #If file already exists in working repository
-    if [[ -e "$3" ]]; then
+    if [[ -e "$file_name" ]]; then
         echo "A file of that name already exists in the project directory"
     #Check if file exists, if so, copy file to project directory
-    elif [[ -e "$2/$3" ]]; then
-        cp $3 ./
+    elif [[ -e "$file_path/$file_name" ]]; then
+        archive
+        cp $file_name ./
     else
-        echo "The file '$3' cannot be found in the file system"
+        echo "The file '$file_name' cannot be found in the file system"
     fi
 }
 
@@ -227,20 +273,22 @@ case $1 in
             file_name="$2"
             newfile=1
             message=""
+            valid=0
             shift 2
             while getopts "cm:" opt; do
                 case ${opt} in
                     c )
-                        newfile=0
-                    ;;
+                        newfile=0 ;;
                     m )
-                        message="${OPTARG}"
-                    ;;
+                        message="${OPTARG}" ;;
                     \? )
-                        echo "Invalid option: -${OPTARG}" >&2;;
+                        valid=1
+                        echo "Invalid option!" >&2;;
                 esac
             done
-            checkin_file "$file_name" "$newfile" "$message"
+            if [ $valid -eq 0 ]; then
+                checkin_file "$file_name" "$newfile" "$message"
+            fi
         fi;;
     "edit")
         if [ $# -lt 2 ]; then
@@ -270,6 +318,14 @@ case $1 in
         show_logs;;
     "show")
         show_repo;;
+    "archives")
+        show_archives;;
+    "revert")
+        if [ $# -lt 2 ]; then
+            echo "Usage: gip revert <archive_name> [-l]"
+        else
+            revert "$2"
+        fi;;
     *)
         echo "Usage: gip <command>"
         echo "Commands:"
